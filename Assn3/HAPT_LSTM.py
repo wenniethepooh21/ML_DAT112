0fb02f3124fc9089607d17b41330fddfae462f2d
Wennie <cutewennie@hotmail.com>
2019-05-30 23:21:56 -0400

    refactored and optimized network

--- Assn3/HAPT_LSTM.py
+++ Assn3/HAPT_LSTM.py
@@ -15,8 +15,9 @@ from sklearn.model_selection import train_test_split
 
 #import dataset 
 print("Reading HAPT file.")
-url = "https://support.scinet.utoronto.ca/~ejspence/HAPT.data.csv"
-HAPTData  = pd.read_csv(url)
+#url = "https://support.scinet.utoronto.ca/~ejspence/HAPT.data.csv"
+file = "./subhapt2.csv"
+HAPTData  = pd.read_csv(file)
 
 #Input data is the time series data
 chunk_size = 50 
@@ -24,12 +25,12 @@ time_series_50 = []
 output_50 = []
 num_features = HAPTData.shape[1] - 1 #to account for ID column
 
+#Separate the data into 50 categorical chunks (same category time sequence)
 i = 0
 while i <= (len(HAPTData) - chunk_size):
 	#Get the action category of the first value of the chunk
 	id_val = HAPTData.loc[i,'ID']
 	temp_50_chunk = HAPTData.loc[i:i+chunk_size -1]
-
 #Check if all values in the chunk are the same as the first value in the chunk 
 	if(temp_50_chunk['ID'].all() == id_val.all()):
 		#The last value in this chunk has the same action category as the first value in the chunk 
@@ -37,88 +38,58 @@ while i <= (len(HAPTData) - chunk_size):
 		inputdf_to_list = temp_50_chunk.drop(columns = 'ID')
 		inputdf_to_list = inputdf_to_list.to_numpy()
 		time_series_50.append(inputdf_to_list)
-
 		outputdf_to_list = temp_50_chunk['ID'].to_numpy()
 		output_50.append(outputdf_to_list)
 		i += 2
 	else:
+                #This 50 chunk is not all the same, determine where the new category starts, set index to that value
 		temp = HAPTData.loc[i:i+chunk_size - 1]
-
 		num_rows_to_drop = temp.loc[temp['ID'] == id_val].shape[0]
 		i += num_rows_to_drop
 
 
-
-
-# #Iterate through the HAPTData set to separate into chunks of 50, to overlap time series, shift down 2
-# for i in range(0,len(HAPTData)-chunk_size,2):
-# 	#Get the action category of the first value of the chunk
-# 	id_val = HAPTData.loc[i,'ID']
-
-# #Check if the 50th value in the chunk is the same as the first value of the same chunk
-# 	if(HAPTData.loc[i+chunk_size -1,'ID'] == id_val):
-# 		#The last value in this chunk has the same action category as the first value in the chunk 
-# 		#Add to time series dataframe 
-# 		inputdf_to_list = HAPTData.loc[i:i+chunk_size-1].drop(columns = 'ID')
-# 		inputdf_to_list = inputdf_to_list.to_numpy()
-# 		time_series_50.append(inputdf_to_list)
-
-# 		outputdf_to_list = HAPTData.loc[i:i+chunk_size - 1]['ID'].to_numpy()
-# 		output_50.append(outputdf_to_list)
-# 	else:
-# 		#The last value in this chunk does not have the same action category as the first value
-# 		#drop the rows that don't have the same action category, it will be in the next chunk 
-# 		temp = HAPTData.loc[i:i+chunk_size - 1]
-# 		# num_rows_to_drop = temp.loc[temp['ID'] != id_val].shape[0]
-# 		# inputdf_to_list = HAPTData.loc[i:i+chunk_size- 1 -num_rows_to_drop].drop(columns = 'ID')
-# 		# inputdf_to_list = inputdf_to_list.to_numpy()
-# 		# time_series_50.append(inputdf_to_list)
-
-# 		# outputdf_to_list = HAPTData.loc[i:i+chunk_size- 1 -num_rows_to_drop]['ID'].to_numpy()
-# 		# output_50.append(outputdf_to_list)
-
-# 		# i += len(inputdf_to_list)
-
-
-# 		num_rows_to_drop = temp.loc[temp['ID'] == id_val].shape[0]
-# 		i += num_rows_to_drop -2
-
-
-#split data into training and testing (70/30)
-#train_test_split function will split the data into training and testing data
-X_train_list, X_test_list, y_train_list, y_test_list = train_test_split(time_series_50, output_50, test_size = 0.3)
+#split data into training and testing (70/30) turn randomizer off, we don't want the data to be randomized
+X_train_list, X_test_list, y_train_list, y_test_list = train_test_split(time_series_50, output_50, test_size = 0.3, random_state = 0)
 
 print("Reshaping the data.")
 
 #reshape the data 
-train_num_50_chunks = len(X_train_list)
+#unravel the list of time series so that we can reshape it to be 3D as the LSTM model expects a 3D input
+#unravel the list of values first, then reshape it
+X_train_unravel = np.concatenate(X_train_list).ravel().ravel()
+#convert from 1D to 3D for keras LSTM 
+X_train = np.reshape(X_train_unravel,(int(len(X_train_unravel)/num_features),1,num_features))
 
-#unravel the list of time series so that we can reshape it to be 3D as the LSTM model expects a 3D x input
-X_train_unravel = np.concatenate(X_train_list).ravel()
-X_train = np.reshape(X_train_unravel,(train_num_50_chunks,chunk_size,num_features))
+#Do the same for the test data set
+X_test_unravel = np.concatenate(X_test_list).ravel().ravel()
+X_test = np.reshape(X_test_unravel,(int(len(X_test_unravel)/num_features),1,num_features))
 
-test_num_50_chunks = len(X_test_list)
-X_test_unravel = np.concatenate(X_test_list).ravel()
-X_test = np.reshape(X_test_unravel,(test_num_50_chunks,chunk_size,num_features))
 
+#format the output values
 y_train_unravel = np.concatenate(y_train_list).ravel()
-y_train = np.reshape(y_train_unravel, (train_num_50_chunks, chunk_size))
+#convert output data into "one hot encoding" format, we have 12 categories
+y_train = ku.to_categorical(y_train_unravel)
 
 y_test_unravel = np.concatenate(y_test_list).ravel()
-y_test = np.reshape(y_test_unravel, (test_num_50_chunks, chunk_size))
+y_test = ku.to_categorical(y_test_unravel)
 
-
+#Build the model
 print("Building the LSTM network.")
-
+output_size = len(HAPTData['ID'].unique()) + 1
 model = km.Sequential()
-model.add(kl.LSTM(100, input_shape = (chunk_size, num_features)))
-model.add(kl.Dense(chunk_size, activation = 'softmax'))
+model.add(kl.LSTM(200, input_shape = (X_train.shape[1], num_features)))
+#add a dropout step to drop 50% of the nodes to sample from
+model.add(kl.Dropout(0.4))
+#add a fully connected layer
+model.add(kl.Dense(100, activation = 'relu')) 
+model.add(kl.Dense(13, activation = 'softmax'))
 
-print("Training the network.")
+#Compile the model
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
-#fit the data to the model 
-fit = model.fit(X_train, y_train, epochs = 50, batch_size = 50, verbose = 2)
+print("Training the network.")
+#fit the data to the model! 
+fit = model.fit(X_train, y_train, epochs = 50, batch_size = 10, verbose = 2)
 
 # evaluates the network on the test data, print out the result
 testScore = model.evaluate(X_test, y_test)
@@ -126,3 +97,8 @@ print("The test score is ", testScore)
 
 # # Save the model so that we can use it as a starting point.
 # model.save('data/' + modelfile)
+#Summarize the history for loss as a function of Epoch
+plt.plot(fit.history['loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
